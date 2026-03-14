5e5a709634f7320741ec0257968f7860292c41fe
TatarArg <raketa5556@yandex.ru>
2026-03-14 13:28:00 +0300

    admin panel for models

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import Ship, ShipPosition
 
-# Register your models here.
+
+class ShipPositionInline(admin.TabularInline):
+    model = ShipPosition
+    extra = 0
+    readonly_fields = ("time", "latitude", "longitude", "speed")
+
+
+@admin.register(Ship)
+class ShipAdmin(admin.ModelAdmin):
+    list_display = ("mmsi", "name")
+    search_fields = ("mmsi", "name")
+    inlines = [ShipPositionInline]
+
+
+@admin.register(ShipPosition)
+class ShipPositionAdmin(admin.ModelAdmin):
+    list_display = ("ship", "time", "latitude", "longitude", "speed")
+    list_filter = ("ship",)
